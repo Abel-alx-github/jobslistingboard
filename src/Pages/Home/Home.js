84bd9695639359cbd2aba09da01dc11a5a70e04a
Abel Tibeso <abeltibeso323@gmail.com>
2024-10-16 19:01:21 +0300

    pages

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css';
+import Hero from '../../Components/Hero/Hero'
+
+const Home = () => {
+  return (
+    <div>
+        <Hero />
+    </div>
+  )
+}
+
+export default Home
