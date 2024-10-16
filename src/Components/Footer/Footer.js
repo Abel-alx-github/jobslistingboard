432be050f08bce733e2321b95a95db79958738b4
Abel Tibeso <abeltibeso323@gmail.com>
2024-10-16 19:00:51 +0300

    components

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className=''>
+        <header className=''>
+            <h2>Footer</h2>
+        </header>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div>
+        <header>
+            <h2>Header</h2>
+        </header>
+    </div>
+  )
+}
+
+export default Header
